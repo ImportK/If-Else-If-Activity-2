344a3f573dd7083f3d44199250802fafdd53cd0d
Kaye Janine Remigio <remigiokayejanine@gmail.com>
2024-10-08 13:24:47 +0800

    Add gradeAssignment.js and index.html

---
+++ gradeAssignment.js
@@ -0,0 +1,15 @@
+// Prompt the user to enter their score.
+function askGrade() {
+    let score = prompt("Enter your score:");
+    if (score >= 90) {
+        alert("Excellent");
+    } else if (score >= 80) {
+        alert("Good");
+    } else if (score >= 70) {
+        alert("Fair");
+    } else if (score < 70) {
+        alert("Needs Improvement");
+    } else {
+        alert("Invalid input. Please enter your score.")
+    }
+}
